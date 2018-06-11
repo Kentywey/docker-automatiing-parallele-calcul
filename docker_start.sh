079b22df66368a2b4ecfc0f86cc590b24bec412d
Kentywey <kweymeesch@gmail.com>
2018-06-11 09:33:25 +0200

    [ADD] Portainer creation
    Auto creation of portainer docker

--- docker_start.sh
+++ docker_start.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 #Build and create as many node server as you wish
 docker build -t node_calcul .;
+read -r -p "Voulez-vous un portainer ?[Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+    docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer
+fi
 echo "How many Node server do you wish ?"
 read number
 i=1
 while((i <= number))
 do
-    docker run -p 4916$i:8080 --name node_calcul$i --volume /var/lib/docker/volumes/node$i/_data -d node_calcul |
+    docker run -p 4916$i:8080 --name node_calcul$i --volume /var/lib/docker/volumes/node$i/_data -d node_calcul | 
         echo "Node server "$i " : state = operating"
     i=`expr $i + 1`
 done
-
 #Display all docker images running
 docker ps
 echo "Congrats, you have created "$number " node server"
